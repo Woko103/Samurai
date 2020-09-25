4dce876d8f2d03bad0e346771ecef60827101c16
Woko103 <43748015+Woko103@users.noreply.github.com>
2020-09-25 18:13:12 +0200

    sonido espadazos

---
+++ Assets/Scripts/Sounds.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sounds : MonoBehaviour
+{
+    public float soundToPlay = -1.0f; //-1 means don't play any sound
+    public AudioClip[] audioClip;
+
+    AudioSource audi;
+
+    void Start ()
+    {
+        audi = GetComponent<AudioSource> ();
+    }
+
+    void Update ()
+    {
+        if (soundToPlay > -1.0f)
+        {
+            PlaySound((int) soundToPlay, 1);
+            soundToPlay = -1.0f;
+        }
+    }
+
+    void PlaySound(int clip, float volumeScale)
+    {
+        audi.PlayOneShot(audioClip [clip], volumeScale);
+    }
+}
